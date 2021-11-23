e9fa75d6ea132ba43c8cb712dcdb62a8d06d6a3d
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-23 17:27:10 +0200

    Create RegisterDto.cs

---
+++ FitMan/DTO/RegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMan.DTO
+{
+    public class RegisterDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
