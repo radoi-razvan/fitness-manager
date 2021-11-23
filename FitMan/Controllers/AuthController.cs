a558cf4419a521b9eec1f847836e16caa320ef3a
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-23 17:29:51 +0200

    Implement authentication using jwt

--- FitMan/Controllers/AuthController.cs
+++ FitMan/Controllers/AuthController.cs
@@ -1,14 +1,94 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using FitMan.Data;
+using FitMan.DTO;
+using FitMan.Models;
+using FitMan.Utils;
+using Microsoft.AspNetCore.Http;
+using System;
 
 namespace FitMan.Controllers
 {
+    [Route("api")]
+    [ApiController]
     public class AuthController : Controller
     {
-        [Route("")]
-        [HttpGet]
-        public IActionResult Register()
+        private readonly IUserRepository _repository;
+        private readonly JwtService _jwtService;
+
+        public AuthController(IUserRepository repository, JwtService jwtService)
         {
-            return Ok("test");
+            _repository = repository;
+            _jwtService = jwtService;
         }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterDto dto)
+        {
+            var user = new User() {
+                Username = dto.Name,
+                Email = dto.Email,
+                Password = PasswordEncrypter.HashPassword(dto.Password)
+            };
+
+            return Created("success", _repository.Create(user));
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginDto dto)
+        {
+            var user = _repository.GetByEmail(dto.Email);
+
+            if (user == null) return BadRequest(new { message = "Invalid Credentials"});
+
+            if (!PasswordEncrypter.VerifyPassword(user.Password, dto.Password))
+            {
+                return BadRequest(new { message = "Invalid Credentials" });
+            }
+
+            var jwt = _jwtService.Generate(user.UserId);
+
+            Response.Cookies.Append("jwt", jwt, new CookieOptions
+            {
+                HttpOnly = true
+            });
+
+            return Ok(new { 
+                message = "success"
+            });
+        }
+
+        [HttpGet("user")]
+        public IActionResult User()
+        {
+            try
+            {
+                var jwt = Request.Cookies["jwt"];
+
+                var token = _jwtService.Verify(jwt);
+
+                int userId = int.Parse(token.Issuer);
+
+                var user = _repository.GetById(userId);
+
+                return Ok(user);
+            }
+            catch (Exception)
+            {
+                return Unauthorized();
+            }
+        }
+
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("jwt");
+
+            return Ok(new
+            {
+                message = "success"
+            });
+        }
+
     }
 }
