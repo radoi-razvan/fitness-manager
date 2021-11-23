aaaa0a8db7142acdad5644a7428b4ab4469f503c
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-23 17:27:20 +0200

    Create IUserRepository.cs

---
+++ FitMan/Data/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using FitMan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMan.Data
+{
+    public interface IUserRepository
+    {
+        User Create(User user);
+        User GetByEmail(string email);
+
+        User GetById(long userId);
+    }
+}
