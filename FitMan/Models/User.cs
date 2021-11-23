87ed36067997fdb6a787729e6c646181c9a75705
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-11-23 14:36:43 +0200

    updated user class from sprint1

---
+++ FitMan/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitMan.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+        public List<Course> Courses{ get; set; }
+
+    }
+}
