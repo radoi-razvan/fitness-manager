4299b7054ee38b3dff71bff14a443f6ce85e6e5c
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-11-23 14:35:25 +0200

    updated course class from sprint1

---
+++ FitMan/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitMan.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long CourseId { get; set; }
+        public string Name { get; set; }
+        public decimal DefaultPrice { get; set; }
+        public string Description { get; set; }
+        public string Schedule { get; set; }
+        public long GymId { get; set; }
+        public List<Review> Reviews { get; set; }
+        public List<User> Participants { get; set; }
+        public List<Trainer> Trainers { get; set; }
+        public List<Exercise> Exercises{ get; set; }
+        public Gym Gym { get; set; }
+    }
+}
