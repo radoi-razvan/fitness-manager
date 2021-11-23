40f58804998c4b191ba5d55f5359d5946367fbc7
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-11-23 14:36:07 +0200

    updated review class from sprint1

---
+++ FitMan/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitMan.Models
+{
+    public class Review
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ReviewId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public long? GymId { get; set; }
+        public long? CourseId { get; set; }
+        public long? TrainerId { get; set; }
+        public Gym Gym { get; set; }
+        public Course Course { get; set; }
+        public Trainer Trainer { get; set; }
+    }
+}
