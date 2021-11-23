66d80505ba5e8934e11469be00d53f5772b8863d
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-11-23 14:33:47 +0200

    Create AuthController.cs

---
+++ FitMan/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitMan.Controllers
+{
+    public class AuthController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return Ok("test");
+        }
+    }
+}
