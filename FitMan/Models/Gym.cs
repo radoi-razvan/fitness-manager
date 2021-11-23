f112c0ecaafe409392782f4d1557145f96aaff32
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-11-23 14:35:56 +0200

    updated gym class from sprint1

---
+++ FitMan/Models/Gym.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitMan.Models
+{
+    public class Gym
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long GymId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public List<Course> Courses { get; set; }
+        public List<Review> Reviews { get; set; }
+
+    }
+}
