eb7cf8409c6fdef6b2b77db96364d3b762c75990
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-23 17:27:47 +0200

    Create PasswordEncrypter.cs

---
+++ FitMan/Utils/PasswordEncrypter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace FitMan.Utils
+{
+    public static class PasswordEncrypter
+    {
+        public static string HashPassword(string password, byte[] salt = null, bool needsOnlyHash = false)
+        {
+            if (salt == null || salt.Length != 16)
+            {
+                // generate a 128-bit salt using a secure PRNG
+                salt = new byte[128 / 8];
+                using (var rng = RandomNumberGenerator.Create())
+                {
+                    rng.GetBytes(salt);
+                }
+            }
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            if (needsOnlyHash) return hashed;
+            // password will be concatenated with salt using ':'
+            return $"{hashed}:{Convert.ToBase64String(salt)}";
+        }
+
+        public static bool VerifyPassword(string hashedPasswordWithSalt, string passwordToCheck)
+        {
+            // retrieve both salt and password from 'hashedPasswordWithSalt'
+            var passwordAndHash = hashedPasswordWithSalt.Split(':');
+            if (passwordAndHash == null || passwordAndHash.Length != 2)
+                return false;
+            var salt = Convert.FromBase64String(passwordAndHash[1]);
+            if (salt == null)
+                return false;
+            // hash the given password
+            var hashOfpasswordToCheck = HashPassword(passwordToCheck, salt, true);
+            // compare both hashes
+            if (String.Compare(passwordAndHash[0], hashOfpasswordToCheck) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
