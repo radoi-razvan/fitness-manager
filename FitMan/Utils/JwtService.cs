9ff3b5de8d326ee12a4ade4b6b1e27f8b89b8255
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-23 17:27:51 +0200

    Create JwtService.cs

---
+++ FitMan/Utils/JwtService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FitMan.Utils
+{
+    public class JwtService
+    {
+        private string _secureKey = "secureandsecretjwtkeyforauthentificationpurposes";
+        public string Generate(long id)
+        {
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secureKey));
+            var credentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            var header = new JwtHeader(credentials);
+
+            // 1 day
+            var payload = new JwtPayload(id.ToString(), null, null, null, DateTime.Today.AddDays(1));
+            var securityToken = new JwtSecurityToken(header, payload);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+
+        public JwtSecurityToken Verify(string jwt)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secureKey);
+            tokenHandler.ValidateToken(jwt, new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false
+            }, out SecurityToken validatedToken);
+
+            return (JwtSecurityToken)validatedToken;
+        }
+
+
+    }
+}
