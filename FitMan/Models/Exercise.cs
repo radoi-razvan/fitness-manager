80c8df2c888a1f8804145ea5e353d1b83ecb3d21
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-11-23 14:35:36 +0200

    updated exercise class from sprint1

---
+++ FitMan/Models/Exercise.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitMan.Models
+{
+    public class Exercise
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ExerciseId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
