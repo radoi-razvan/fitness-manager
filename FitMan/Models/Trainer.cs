04ce2cd266cf80165d4566f0e24a923078f0b1f1
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-11-23 14:36:21 +0200

    updated trainer class from sprint1

---
+++ FitMan/Models/Trainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitMan.Models
+{
+    public class Trainer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long TrainerId { get; set; }
+        public string Name { get; set; }
+        public int ExperienceYears { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public long CourseId { get; set; }
+        public Course Course { get; set; }
+        public List<Review> Reviews { get; set; }
+
+    }
+}
