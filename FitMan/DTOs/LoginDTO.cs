1e4389a0fcd096bc71ad2ddd095e2880be1ea9c8
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-24 15:27:01 +0200

    Create LoginDTO.cs

---
+++ FitMan/DTOs/LoginDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMan.DTOs
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
