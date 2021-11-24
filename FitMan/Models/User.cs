b3a7f4c6744de9e70d5e0a62cdf3a0edf5310e3b
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-24 15:28:42 +0200

    Update User.cs

--- FitMan/Models/User.cs
+++ FitMan/Models/User.cs
@@ -14,6 +14,7 @@ namespace FitMan.Models
         public string Password { get; set; }
         public UserRole Role { get; set; }
         public List<Course> Courses{ get; set; }
+        public List<Gym> Gyms { get; set; }
 
     }
 }
