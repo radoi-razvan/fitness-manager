02a8f003e6fd547fd69c45621d4fc3a6ae5b1ea9
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-25 16:57:34 +0200

    Update App.js

--- fit-man/src/App.js
+++ fit-man/src/App.js
@@ -1,15 +1,32 @@
-import './App.css';
-import { Footer } from './components/common/Footer';
-import { Navbar } from './components/common/Navbar';
+import "./App.css";
+import { Footer } from "./components/common/Footer";
+import { Navbar } from "./components/common/Navbar";
+import { GymList } from "./components/GymList";
+import { WelcomePage } from "./components/WelcomePage";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  NavLink,
+  Link,
+  useLocation,
+} from "react-router-dom";
 
 function App() {
+  // const location = useLocation();
+
   return (
     <div className="container">
-        <Navbar />
-        <main id="main">
-          Welcome to Fitness Manager!
-        </main>
-        <Footer />
+      <Navbar />
+      <main id="main">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={WelcomePage} />
+            <Route exact path="/api/gyms" component={GymList} />
+          </Switch>
+        </BrowserRouter>
+      </main>
+      <Footer />
     </div>
   );
 }
