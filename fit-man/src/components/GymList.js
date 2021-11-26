bfbdd23fdb5ffb729dde59db525d97cb0baba11d
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-26 11:46:59 +0200

    Update GymList.js

--- fit-man/src/components/GymList.js
+++ fit-man/src/components/GymList.js
@@ -3,7 +3,9 @@ import { Gym } from "./Gym";
 import { STATE } from "./State";
 import { useAtom } from "jotai";
 import axios from "axios";
+import { apiRoute } from "../DataManager/ApiRoute";
 import { useState, useEffect } from "react";
+import { dataHandler } from "../DataManager/DataHandler";
 
 export const GymList = () => {
   const [gyms, setGyms] = useAtom(STATE.GYMS);
@@ -16,12 +18,20 @@ export const GymList = () => {
     //     setGyms(response.data);
     //   })
     //   .catch((e) => console.error(e));
-    getGyms();
+
+    //getGyms();
+    getGymsFromDataHandler();
+
   }, []);
 
-  const getGyms = async () => {
-    const response = await axios.get("http://localhost:5000/api/gyms");
-    setGyms(response.data);
+  // const getGyms = async () => {
+  //   const response = await axios.get(`${apiRoute}/api/gyms`);
+  //   setGyms(response.data);
+  // };
+
+  const getGymsFromDataHandler = async () => {
+    const data = await dataHandler.getGyms();
+    setGyms(data);
   };
 
   return (
