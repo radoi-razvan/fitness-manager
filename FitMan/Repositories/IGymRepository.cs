178d0a325dfabe8f06a3cbcb96cd46b4f74e2562
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-30 00:35:29 +0200

    Create IGymRepository.cs

---
+++ FitMan/Repositories/IGymRepository.cs
@@ -0,0 +1,14 @@
+﻿using FitMan.Data;
+using FitMan.DTOs;
+using FitMan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMan.Repositories
+{
+    public interface IGymRepository : IRepository<GymDTO>
+    {
+    }
+}
