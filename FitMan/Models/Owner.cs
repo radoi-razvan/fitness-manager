70b0389b8bdc615d49d9d4530daccb1fa0dd782b
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-11-29 14:42:46 +0200

    Created Participant and Owner Roles

---
+++ FitMan/Models/Owner.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace FitMan.Models
+{
+    public class Owner : User
+    { 
+        public List<Gym> Gyms { get; set; }
+    }
+}
---
+++ FitMan/Models/Participant.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace FitMan.Models
+{
+    public class Participant : User
+    {
+        public List<Course> Courses{ get; set; }
+    }
+}
