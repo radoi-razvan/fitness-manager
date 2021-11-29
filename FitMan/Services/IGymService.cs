610bb7802451c8a0c8ff72450c1aa041e016aac1
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-29 11:32:03 +0200

    Create IGymService.cs

---
+++ FitMan/Services/IGymService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FitMan.DTOs;
+using FitMan.Models;
+
+namespace FitMan.Services
+{
+    public interface IGymService : IService<GymDTO>
+    {
+    }
+}
