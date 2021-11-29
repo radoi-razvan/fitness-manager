de6995d1d84b6dc2ba3df1b2c807d6cf54fa8ef2
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-29 11:32:10 +0200

    Create GymService.cs

---
+++ FitMan/Services/GymService.cs
@@ -0,0 +1,49 @@
+﻿using FitMan.Data;
+using FitMan.DTOs;
+using FitMan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMan.Services
+{
+    public class GymService : IGymService
+    {
+        private readonly IGymRepository _gymRepository;
+        public GymService(IGymRepository gymRepository)
+        {
+            _gymRepository = gymRepository;
+        }
+
+        public void Add(GymDTO item)
+        {
+            _gymRepository.Add(item);
+        }
+
+        public bool CheckIfExists(long id)
+        {
+            return _gymRepository.CheckIfExists(id);
+        }
+
+        public GymDTO Get(long id)
+        {
+            return _gymRepository.Get(id);
+        }
+
+        public IEnumerable<GymDTO> GetAll()
+        {
+            return _gymRepository.GetAll();
+        }
+
+        public void Remove(GymDTO item)
+        {
+            _gymRepository.Remove(item);
+        }
+
+        public bool Update(long id, GymDTO item)
+        {
+            return _gymRepository.Update(id, item);
+        }
+    }
+}
