3f8cb2aa9eb6bcd43da0f154c946b5a43733b420
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-30 00:35:34 +0200

    Create IRepository.cs

---
+++ FitMan/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMan.Repositories
+{
+    public interface IRepository<T>
+    {
+        void Add(T item);
+        void Remove(T item);
+        T Get(long id);
+        IEnumerable<T> GetAll();
+        bool CheckIfExists(long id);
+        bool Update(long id, T item);
+    }
+}
