9183c8a7a6fc15cff42bc438b0c1c312c87fe6f3
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-30 00:35:27 +0200

    Create GymRepository.cs

---
+++ FitMan/Repositories/GymRepository.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using FitMan.Data;
+using FitMan.DTOs;
+using FitMan.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMan.Repositories
+{
+    public class GymRepository : IGymRepository
+    {
+        private readonly ApplicationContext _context;
+        private readonly IMapper _mapper;
+
+        public GymRepository(ApplicationContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public void Add(GymDTO item)
+        {
+            var model = _mapper.Map<Gym>(item);
+            _context.Gyms.Add(model);
+            _context.SaveChanges();
+        }
+
+        public bool CheckIfExists(long id)
+        {
+            return _context.Gyms.Any(e => e.GymId == id);
+        }
+
+        public GymDTO Get(long id)
+        {
+            var model = _context.Gyms.Find(id);
+            var dto = _mapper.Map<GymDTO>(model);
+            return dto;
+        }
+
+        public IEnumerable<GymDTO> GetAll()
+        {
+            return _mapper.Map<IEnumerable<GymDTO>>(_context.Gyms);  
+        }
+
+        public void Remove(GymDTO item)
+        {
+            var gym = _context.Gyms.Find(item.GymId);
+            _context.Gyms.Remove(gym);
+            _context.SaveChanges();
+        }
+
+        public bool Update(long id, GymDTO item)
+        {
+
+            try
+            {
+                Gym gym = _context.Gyms.Find(id);
+                gym.Name = item.Name;
+                gym.Address = item.Address;
+                gym.Description = item.Description;
+
+               _context.SaveChanges();
+                return true;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CheckIfExists(id))
+                {
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+
+            }
+        }
+
+
+    }
+}
