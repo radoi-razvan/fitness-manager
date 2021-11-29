f084f49394b9226f887949fe01c10a815d9b9087
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-30 00:35:41 +0200

    Create UserRepository.cs

---
+++ FitMan/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using FitMan.Data;
+using FitMan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FitMan.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public UserRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public User Create(User user)
+        {
+            _context.Users.Add(user);
+            user.UserId = _context.SaveChanges();
+
+            return user;
+        }
+
+        public User GetByEmail(string email)
+        {
+            return _context.Users.FirstOrDefault(u => u.Email == email);
+        }
+
+        public User GetById(long userId)
+        {
+            return _context.Users.FirstOrDefault(u => u.UserId == userId);
+        }
+    }
+}
