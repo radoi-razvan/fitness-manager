df2403544ce163079fbc5ee6ee2e684955f95c44
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-29 11:30:25 +0200

    Create GymDTO.cs

---
+++ FitMan/DTOs/GymDTO.cs
@@ -0,0 +1,19 @@
+﻿using FitMan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMan.DTOs
+{
+    public class GymDTO
+    {
+        public long GymId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public List<Course> Courses { get; set; }
+        public List<Review> Reviews { get; set; }
+        public List<User> User { get; set; }
+    }
+}
