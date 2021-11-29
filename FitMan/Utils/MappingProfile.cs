6bc58be12cff4fdf5796f41a6c44d4b3a5930171
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-11-29 11:31:03 +0200

    Create MappingProfile.cs

---
+++ FitMan/Utils/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FitMan.DTOs;
+using FitMan.Models;
+
+namespace FitMan.Utils
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Gym, GymDTO>();
+            CreateMap<GymDTO, Gym>();
+        }
+    }
+}
