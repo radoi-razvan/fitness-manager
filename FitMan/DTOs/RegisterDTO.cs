8ba1d9353b684a4a2b9e625052fd22576c9a871b
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-11-29 14:43:39 +0200

    Update RegisterDTO.cs

--- FitMan/DTOs/RegisterDTO.cs
+++ FitMan/DTOs/RegisterDTO.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace FitMan.DTOs
 {
     public class RegisterDTO
@@ -10,5 +6,7 @@ namespace FitMan.DTOs
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string UserRole { get; set; }
+
     }
 }
