8a6bfd7f514b56048bc36978e6b93d8f01d9c6f7
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-12-06 14:34:36 +0200

    Update WelcomePage.js

--- fit-man/src/components/WelcomePage.js
+++ fit-man/src/components/WelcomePage.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 export const WelcomePage = () => {
     return (
-        <div className="mycard txt-primary">
-            <p>Welcome to Fitness Manager!</p>
+        <div className="video-overlay main-banner" id="top">
+            <div className="header-text">
+                <div className="caption">
+                <h2>WELCOME TO <em>FITMAN</em></h2>
+                <h6>The only fitness manager you'll ever need</h6>
+                </div>      
+            </div>
         </div>
     )
 }
+
