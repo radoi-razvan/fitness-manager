62781ac0601fc956d5b44f670b3598c59f8d62d8
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-12-07 19:50:30 +0200

    removed useless imports

--- FitMan/Startup.cs
+++ FitMan/Startup.cs
@@ -1,23 +1,15 @@
 using AutoMapper;
 using FitMan.Data;
-using FitMan.Models;
 using FitMan.Data.Repositories;
 using FitMan.Services;
 using FitMan.Utils;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using FitMan.Data.Repositories.Interfaces;
 using FitMan.Services.Interfaces;
 
