f89c4a83b3a739265eeca639de54a5bed87c1ba2
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-12-07 23:15:45 +0200

    Implement courses display

--- fit-man/src/components/Course.js
+++ fit-man/src/components/Course.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { useParams } from "react-router";
+import { NavLink } from "react-router-dom";
 
-export const Course = ({ name, address, description }) => {
-  let gymId = useParams();
+export const Course = ({
+  name,
+  defaultPrice,
+  description,
+  schedule,
+  courseId,
+}) => {
+
+  let params = useParams();
+
   return (
     <div className="trainer-item backdrop-item">
-
       <span className="">{name}</span>
-      <p className="">{address}</p>
+      <p className="">$ {defaultPrice}</p>
       <h4 className="">{description}</h4>
+      <p>{schedule}</p>
+      <NavLink to={`/gyms/${params.gymId}/courses/${courseId}/exercises`}>
+        Exercises
+      </NavLink>
+      <NavLink to={`/gyms/${params.gymId}/courses/${courseId}/trainers`}>
+        Trainers
+      </NavLink>
     </div>
   );
 };
--- fit-man/src/components/CourseList.js
+++ fit-man/src/components/CourseList.js
@@ -2,42 +2,35 @@ import React from "react";
 import { Course } from "./Course";
 import { STATE } from "./State";
 import { useAtom } from "jotai";
-import axios from "axios";
 import { useEffect } from "react";
 import { dataHandler } from "../DataManager/DataHandler";
+import { useParams } from "react-router";
 
 export const CourseList = () => {
   const [courses, setCourses] = useAtom(STATE.COURSES);
+  let params = useParams();
 
   useEffect(() => {
     getCourses();
-  });
+  }, []);
 
   const getCourses = async () => {
-    const response = await dataHandler.getCourses();
+    const response = await dataHandler.getCourses(params.gymId);
     setCourses(response.data);
   };
 
   return (
     <div>
-        {/* <div className="section-heading">
-            <h2><em>{name}</em></h2>
-        </div> */}
-        <div>
-          {courses.map((course, index) => {
-            <Course
-              name={course.Name}
-              defaultPrice={course.DefaultPrice}
-              description={course.Description}
-              schedule={course.Schedule}
-              courseId={course.CourseId}
-              key={index}
-            />
-          })}
-        </div>
-        {/* <div>
-            <em> {address} </em>
-        </div> */}
+      {courses.map((course, index) => (
+        <Course
+          name={course.Name}
+          defaultPrice={course.DefaultPrice}
+          description={course.Description}
+          schedule={course.Schedule}
+          courseId={course.CourseId}
+          key={index}
+        />
+      ))}
     </div>
   );
-}
+};
