e635dd7951d6cdc68dfca2d3490b864e3b7dfbaf
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-12-07 19:53:56 +0200

    add links to gym names

--- fit-man/src/components/Gym.js
+++ fit-man/src/components/Gym.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-export const Gym = ({ name, address, description }) => {
+export const Gym = ({ gymId, name, address, description }) => {
   return (
     <div className="trainer-item backdrop-item">
-      <span className="">{name}</span>
+      <span className=""><NavLink to={`/gyms/${gymId}/courses`}>{name}</NavLink></span>
       <p className="">{address}</p>
       <h4 className="">{description}</h4>
     </div>
