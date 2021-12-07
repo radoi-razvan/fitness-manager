6ce5b39fa047c77df99b6cbdb07d56f09ed0fbf1
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-12-07 19:54:20 +0200

    Update Footer.js

--- fit-man/src/components/common/Footer.js
+++ fit-man/src/components/common/Footer.js
@@ -2,16 +2,9 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <p>
-            &copy; {new Date().getFullYear()} - <span>FitMan</span>
-            </p>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <div>
+      
+    </div>
+
   );
 };
