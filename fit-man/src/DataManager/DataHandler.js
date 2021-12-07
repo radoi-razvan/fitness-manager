310ffc076a75c94dcdc77db6338bbdbf28f815b1
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-12-07 23:15:04 +0200

    Update DataHandler.js

--- fit-man/src/DataManager/DataHandler.js
+++ fit-man/src/DataManager/DataHandler.js
@@ -7,8 +7,41 @@ export const dataHandler = {
     return response;
   },
 
-  getCourses: async function(GymId) {
-    const response = await axios.get(`${apiRoute}/gyms/${GymId}/courses`);
-    return response; 
-  }
+  getCourses: async function (gymId) {
+    const response = await axios.get(`${apiRoute}/gyms/${gymId}/courses`);
+    return response;
+  },
+
+  getExercises: async function (gymId, courseId) {
+    const response = await axios.get(
+      `${apiRoute}/gyms/${gymId}/courses/${courseId}/exercises`
+    );
+    return response;
+  },
+
+  getTrainers: async function (gymId, courseId) {
+    const response = await axios.get(
+      `${apiRoute}/gyms/${gymId}/courses/${courseId}/trainers`
+    );
+    return response;
+  },
+
+  getGymReviews: async function (gymId) {
+    const response = await axios.get(`${apiRoute}/gyms/${gymId}/reviews`);
+    return response;
+  },
+
+  getCourseReviews: async function (gymId, courseId) {
+    const response = await axios.get(
+      `${apiRoute}/gyms/${gymId}/courses/${courseId}/reviews`
+    );
+    return response;
+  },
+
+  getTrainerReviews: async function (gymId, courseId, trainerId) {
+    const response = await axios.get(
+      `${apiRoute}/gyms/${gymId}/courses/${courseId}/trainers/${trainerId}/reviews`
+    );
+    return response;
+  },
 };
