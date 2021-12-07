f11b29d0b1eea5a6d297cd0d12034a71a9a7352e
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-12-07 23:17:06 +0200

    Update App.js

--- fit-man/src/App.js
+++ fit-man/src/App.js
@@ -1,37 +1,44 @@
-import React from 'react';
+import React from "react";
 import "./css/App.css";
 import { Navbar } from "./components/common/Navbar";
 import { Footer } from "./components/common/Footer";
-import { GymList } from './components/GymList';
-import { WelcomePage } from './components/WelcomePage';
+import { GymList } from "./components/GymList";
+import { WelcomePage } from "./components/WelcomePage";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { CourseList } from './components/CourseList';
+import { CourseList } from "./components/CourseList";
+import { ExerciseList } from "./components/ExerciseList";
+import { TrainerList } from "./components/TrainerList";
 
 function App() {
   // const location = useLocation();
-  // TODO apiGet, apiPost, apiPut, apiDelete file
-  
+
   return (
     <>
-    <div className="container">
-      <BrowserRouter>
-        <Navbar />
-        <main id="main">
-          <Switch>
-            <Route exact path="/">
+      <div className="container">
+        <BrowserRouter>
+          <Navbar />
+          <main id="main">
+            <Switch>
+              <Route exact path="/">
                 <WelcomePage />
-            </Route>
-            <Route path="/gyms/:gymId/courses">
-              <CourseList />
-            </Route>
-            <Route path="/gyms">
-              <GymList />
-            </Route>
-          </Switch>
-        </main>
-      </BrowserRouter>
-    </div>
-    {/* <Footer /> */}
+              </Route>
+              <Route path="/gyms/:gymId/courses/:courseId/trainers">
+                <TrainerList />
+              </Route>
+              <Route path="/gyms/:gymId/courses/:courseId/exercises">
+                <ExerciseList />
+              </Route>
+              <Route path="/gyms/:gymId/courses">
+                <CourseList />
+              </Route>
+              <Route path="/gyms">
+                <GymList />
+              </Route>
+            </Switch>
+          </main>
+        </BrowserRouter>
+      </div>
+      {/* <Footer /> */}
     </>
   );
 }
