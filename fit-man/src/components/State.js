46a8e6a64565696574e85b19541de9b2ec55f1f7
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-12-07 23:14:58 +0200

    Update State.js

--- fit-man/src/components/State.js
+++ fit-man/src/components/State.js
@@ -1,5 +1,11 @@
 import { atom } from "jotai";
 
 export const STATE = {
-    GYMS: atom([])
+  GYMS: atom([]),
+  COURSES: atom([]),
+  EXERCISES: atom([]),
+  TRAINERS: atom([]),
+  GYM_REVIEWS: atom([]),
+  COURSE_REVIEWS: atom([]),
+  TRAINER_REVIEWS: atom([]),
 };
