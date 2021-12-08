552bdd273cf20d10f41c98424fe85de1daa0cf9f
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-12-08 21:21:48 +0200

    Update DataHandler.js

--- fit-man/src/DataManager/DataHandler.js
+++ fit-man/src/DataManager/DataHandler.js
@@ -1,52 +1,52 @@
-import { apiRoute } from "../DataManager/ApiRoute";
 import axios from "axios";
 
 export const dataHandler = {
   getGyms: async function () {
-    const response = await axios.get(`${apiRoute}/gyms`);
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND}/gyms`);
     return response;
   },
 
   getCourses: async function (gymId) {
-    const response = await axios.get(`${apiRoute}/gyms/${gymId}/courses`);
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND}/gyms/${gymId}/courses`);
     return response;
   },
 
   getExercises: async function (gymId, courseId) {
     const response = await axios.get(
-      `${apiRoute}/gyms/${gymId}/courses/${courseId}/exercises`
+      `${process.env.REACT_APP_BACKEND}/gyms/${gymId}/courses/${courseId}/exercises`
     );
     return response;
   },
 
   getTrainers: async function (gymId, courseId) {
     const response = await axios.get(
-      `${apiRoute}/gyms/${gymId}/courses/${courseId}/trainers`
+      `${process.env.REACT_APP_BACKEND}/gyms/${gymId}/courses/${courseId}/trainers`
     );
     return response;
   },
 
   getGymReviews: async function (gymId) {
-    const response = await axios.get(`${apiRoute}/gyms/${gymId}/reviews`);
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND}/gyms/${gymId}/reviews`);
     return response;
   },
 
   getCourseReviews: async function (gymId, courseId) {
     const response = await axios.get(
-      `${apiRoute}/gyms/${gymId}/courses/${courseId}/reviews`
+      `${process.env.REACT_APP_BACKEND}/gyms/${gymId}/courses/${courseId}/reviews`
     );
     return response;
   },
 
   getTrainerReviews: async function (gymId, courseId, trainerId) {
+
     const response = await axios.get(
-      `${apiRoute}/gyms/${gymId}/courses/${courseId}/trainers/${trainerId}/reviews`
+      `${process.env.REACT_APP_BACKEND}/gyms/${gymId}/courses/${courseId}/trainers/${trainerId}/reviews`
     );
     return response;
   },
 
   getAllReviews: async function () {
-    const response = await axios.get(`${apiRoute}/gyms/reviews`);
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND}/gyms/reviews`);
     return response;
   },
 };
