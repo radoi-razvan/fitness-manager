c611eceb2ef22afc0640f893e1092ce157ce9c87
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-12-09 18:44:45 +0200

    Update Trainer.js

--- fit-man/src/components/Trainer.js
+++ fit-man/src/components/Trainer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Trainer = ({ name, experienceYears, dateOfBirth }) => {
   return (
     <div className="trainer-item backdrop-item">
-      {/* <img src={`${process.env.REACT_APP_IMG}/${name}.jpg`} alt={name} /> */}
+      <img className="gym-logo" src={`${process.env.REACT_APP_BASEIMGURL}${process.env.REACT_APP_TRAINERIMG}/${name}.png`} alt={name} />
       <span>{name}</span>
       <p>Experience: {experienceYears} years</p>
       <p>{dateOfBirth}</p>
