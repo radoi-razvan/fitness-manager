a8ac514d3212245cef05930d31902d983685a377
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-12-09 12:57:19 +0200

    Update Gym.js

--- fit-man/src/components/Gym.js
+++ fit-man/src/components/Gym.js
@@ -5,13 +5,22 @@ import { GymReviewList } from "./GymReviewList";
 export const Gym = ({ gymId, name, address, description }) => {
   return (
     <div className="trainer-item backdrop-item">
-      <span className="">
-        <NavLink to={`/gyms/${gymId}/courses`}>{name}</NavLink>
-      </span>
-      {/* <img src={`${process.env.REACT_APP_IMG}/${name}.jpg`} alt={name} /> */}
-      <p className="">{address}</p>
-      <h4 className="">{description}</h4>
-      {/* <GymReviewList gymId={gymId} /> */}
+      <div className="image-thumb">
+        <img className="gym-logo" src={`${process.env.REACT_APP_GYM_IMG}/${name}.png`} alt={name}/>
+      </div>
+      <div className="down-content">
+        <span className="">
+          <NavLink to={`/gyms/${gymId}/courses`}>{name}</NavLink>
+        </span>
+        <p className="">{address}</p>
+        <h4 className="">{description}</h4>
+        {/* <GymReviewList gymId={gymId} /> */}
+        <ul className="social-icons">
+          <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+          <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+        </ul>
+      </div>
     </div>
   );
 };
