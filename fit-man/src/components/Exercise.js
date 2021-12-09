9e77c023045af3f961365999cc1f0865f99a1d2f
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-12-09 19:23:15 +0200

    Update Exercise.js

--- fit-man/src/components/Exercise.js
+++ fit-man/src/components/Exercise.js
@@ -3,9 +3,11 @@ import React from "react";
 export const Exercise = ({ name, description }) => {
 
   return (
-    <div className="trainer-item backdrop-item">
-      <span className="">{name}</span>
-      <h4 className="">{description}</h4>
+    <div className="card-item">
+      <div className="card-text">
+        <span className="">{name}</span>
+        <h4 className="">{description}</h4>
+      </div>
     </div>
   );
 };
