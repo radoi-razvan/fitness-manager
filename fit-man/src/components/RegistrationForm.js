39dfa16e0d70ce6c8518c640d35e92cc7b366ace
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-12-10 10:35:39 +0200

    updated forms position

--- fit-man/src/components/RegistrationForm.js
+++ fit-man/src/components/RegistrationForm.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export const RegistrationForm = () => {
   return (
-    <section className="vh-100">
+    <section className="vh-90">
       <div className="container h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-lg-12 col-xl-11">
             <div className="trainer-item backdrop-item text-black">
-              <div className="card-body p-md-5" id="register-body">
+              <div className="card-body  p-md-5" id="register-body">
                 <div className="row justify-content-center">
                   <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                     <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 txt-main-color">
