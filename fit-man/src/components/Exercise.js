427e189ce1d591b8c76ee1486f5e570c45dc2eae
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2021-12-15 15:04:35 +0200

    Update Exercise.js

--- fit-man/src/components/Exercise.js
+++ fit-man/src/components/Exercise.js
@@ -17,22 +17,19 @@ export const Exercise = ({ exerciseId, name, description }) => {
     const response = await dataHandler.deleteExercise(params.gymId, params.courseId, exerciseId);
 
     typeof response !== "undefined" && response.status === 204
-      ? history.push(`/gyms/${params.gymId}/courses/${params.courseId}/exercises`)
+      ? history.go(0)
       : history.push(location);
   };
 
   return (
     <div className="card-item">
       <div className="card-text">
-        <span className="">{name}</span>
+        <span className="">
+          {name}
+          <NavLink className="bi bi-pencil-square ms-3 btn-icon" exact to={`/gyms/${params.gymId}/courses/${params.courseId}/exercises/${exerciseId}/edit`}/>
+          <i className="delete-icon bi bi-trash-fill ms-3 btn-icon" onClick={(e) => deleteEvent(e)}/>
+        </span>
         <h4 className="">{description}</h4>
-        <NavLink
-          exact
-          to={`/gyms/${params.gymId}/courses/${params.courseId}/exercises/${exerciseId}/edit`}
-        >
-          Edit Exercise
-        </NavLink>
-        <button onClick={(e) => deleteEvent(e)}>Delete Exercise</button>
       </div>
     </div>
   );
