6d4589b9e34361da9d63d09e0d5b3d464d35065b
radoi-razvan <r4zzzr4zzz@gmail.com>
2021-12-15 11:53:39 +0200

    Implement gym form

--- fit-man/src/components/Gym.js
+++ fit-man/src/components/Gym.js
@@ -5,7 +5,10 @@ import { NavLink } from "react-router-dom";
 export const Gym = ({ gymId, name, address, description }) => {
   return (
     <div className="card-item">
-      <img src={`${process.env.REACT_APP_BASEIMGURL}${process.env.REACT_APP_GYMIMG}/${name}.png`} alt={name}/>
+      <img
+        src={`${process.env.REACT_APP_BASEIMGURL}${process.env.REACT_APP_GYMIMG}/${name}.png`}
+        alt={name}
+      />
       <div className="card-text">
         <span>
           <NavLink to={`/gyms/${gymId}/courses`}>{name}</NavLink>
@@ -13,13 +16,27 @@ export const Gym = ({ gymId, name, address, description }) => {
         <p>{address}</p>
         <h4>{description}</h4>
         {/* <GymReviewList gymId={gymId} /> */}
+        <NavLink exact to={`/gyms/${gymId}/edit`}>
+          Edit Gym
+        </NavLink>
         <ul className="social-icons">
-          <li><a href={`${process.env.REACT_APP_FRONTEND}`}><i className="bi bi-facebook"></i></a></li>
-          <li><a href={`${process.env.REACT_APP_FRONTEND}`}><i className="bi bi-instagram"></i></a></li>
-          <li><a href={`${process.env.REACT_APP_FRONTEND}`}><i className="bi bi-twitter"></i></a></li>
+          <li>
+            <a href={`${process.env.REACT_APP_FRONTEND}`}>
+              <i className="bi bi-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a href={`${process.env.REACT_APP_FRONTEND}`}>
+              <i className="bi bi-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a href={`${process.env.REACT_APP_FRONTEND}`}>
+              <i className="bi bi-twitter"></i>
+            </a>
+          </li>
         </ul>
       </div>
     </div>
-
   );
 };
