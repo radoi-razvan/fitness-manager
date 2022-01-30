114e105094ccafd6a206edf0c31b2beeab6bdc84
radoi-razvan <r4zzzr4zzz@gmail.com>
2022-01-30 21:43:53 +0200

    Add get attended courses method to service interface

--- FitMan/Services/Interfaces/IParticipantService.cs
+++ FitMan/Services/Interfaces/IParticipantService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FitMan.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,5 +11,6 @@ namespace FitMan.Services.Interfaces
         public int GetTotalGymMembers(long gymId);
         public int GetTotalCourseMembers(long courseId);
         public void Add(long courseId, long userId);
+        public IEnumerable<CourseDTO> GetAttendedCourses(long participantId);
     }
 }
