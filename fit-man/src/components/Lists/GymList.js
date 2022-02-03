805f8fc761218dc6dc965e4fde5c60231914187f
AlexM <51971295+AlexMoraru97@users.noreply.github.com>
2022-02-03 12:21:25 +0200

    add show owner only gyms functionality

--- fit-man/src/components/Lists/GymList.js
+++ fit-man/src/components/Lists/GymList.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Gym } from "../Gym";
-import { STATE, userSetter, loggedInSetter } from "../State";
+import { STATE, userSetter, loggedInSetter, ownedGymsSetter } from "../State";
 import { useAtom } from "jotai";
-import { useUpdateAtom } from "jotai/utils";
+import { useAtomValue, useUpdateAtom } from "jotai/utils";
 import { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
@@ -10,33 +10,72 @@ export const GymList = () => {
   const [gyms, setGyms] = useAtom(STATE.GYMS);
   const [user] = useAtom(userSetter);
   const [loggedIn] = useAtom(loggedInSetter);
+  const ownedGyms = useAtomValue(ownedGymsSetter);
   const setTotalGymsMembers = useUpdateAtom(STATE.GYMS_MEMBERS);
+  const [myGymsCheck, setMyGymsCheck] = useAtom(STATE.GYMS_CHECKER);
 
   useEffect(() => {
     setGyms();
     setTotalGymsMembers();
   }, []);
 
+  const setCheckText = (isChecked) => {
+    const label = document.getElementById("checkboxLabel");
+    label.innerHTML = isChecked ? `My <em>Gyms</em>` : `All <em>Gyms</em>`;
+  };
+
   return (
     <div id="trainers">
-      <NavLink
-        className={`btn-1 btn-fixed-left ${
-          !loggedIn || !("Gyms" in user) ? "logout-display" : ""
-        }`}
-        exact
-        to="/gyms/add"
-      >
-        Add <em>Gym</em>
-      </NavLink>
-      {gyms.map((gym, index) => (
-        <Gym
-          name={gym.Name}
-          address={gym.Address}
-          description={gym.Description}
-          gymId={gym.GymId}
-          key={index}
-        />
-      ))}
+      {loggedIn && "Gyms" in user ? (
+        <>
+          <NavLink className="btn-1 btn-fixed-left" exact to="/gyms/add">
+            Add <em>Gym</em>
+          </NavLink>
+          <div className="area-fixed-right">
+            <input
+              type="checkbox"
+              className="btn-check"
+              id="btn-check-outlined"
+              autoComplete="off"
+              onClick={(e) => {
+                setMyGymsCheck(e.target.checked);
+                setCheckText(e.target.checked);
+              }}
+            />
+            <label
+              className="btn btn-outline-primary styled-outline-btn"
+              htmlFor="btn-check-outlined"
+              id="checkboxLabel"
+            >
+              All <em>Gyms</em>
+            </label>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
+
+      {myGymsCheck
+        ? gyms
+            .filter((gym) => ownedGyms.includes(gym.GymId))
+            .map((gym, index) => (
+              <Gym
+                name={gym.Name}
+                address={gym.Address}
+                description={gym.Description}
+                gymId={gym.GymId}
+                key={index}
+              />
+            ))
+        : gyms.map((gym, index) => (
+            <Gym
+              name={gym.Name}
+              address={gym.Address}
+              description={gym.Description}
+              gymId={gym.GymId}
+              key={index}
+            />
+          ))}
     </div>
   );
 };
